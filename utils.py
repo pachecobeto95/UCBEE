5aa89b947cdf825082b269e6a0c9b582e5b88757
Roberto Pacheco <robertopvc@gmail.com>
2022-10-23 18:33:57 -0300

    a

--- utils.py
+++ utils.py
@@ -106,8 +106,8 @@ def load_caltech256(args, dataset_path, save_indices_path, distortion_lvl):
 	val_set = datasets.ImageFolder(dataset_path, transform=transformations_test)
 	test_set = datasets.ImageFolder(dataset_path, transform=transformations_test)
 
-	train_idx_path = os.path.join(save_indices_path, "training_idx_caltech256.npy")
-	val_idx_path = os.path.join(save_indices_path, "validation_idx_caltech256.npy")
+	train_idx_path = os.path.join(save_indices_path, "training_idx_caltech256_%s.npy"%(args.model_id))
+	val_idx_path = os.path.join(save_indices_path, "validation_idx_caltech256_%s.npy"%(args.model_id))
 	#test_idx_path = os.path.join(save_indices_path, "test_idx_caltech256.npy")
 
 	if( os.path.exists(train_idx_path) ):
