ea5b38945a9bfabb54506c8304571f499f96fc7a
Roberto Pacheco <robertopvc@gmail.com>
2022-10-23 12:46:00 -0300

    adding first files

--- run_ucb_experiment.py
+++ run_ucb_experiment.py
@@ -12,21 +12,30 @@ if __name__ == "__main__":
 	parser.add_argument('--c', type=float, default=config.c, help='Parameter c.')
 	parser.add_argument('--n_rounds', type=int, default=config.n_rounds, help='Model Id (default: %s)'%(config.n_rounds))
 	parser.add_argument('--distortion_type', type=str, default=config.distortion_type, help='Distortion Type.')
+	parser.add_argument('--n_branches', type=int, default=config.n_branches, help='Number of exit exits.')
+	parser.add_argument('--dataset_name', type=str, default=config.dataset_name, help='Dataset Name.')
+	parser.add_argument('--model_name', type=str, default=config.model_name, help='Model name.')
+	parser.add_argument('--seed', type=int, default=config.seed, help='Seed.')
+	parser.add_argument('--calib_type', type=str, default="no_calib", help='Calibration Type.')
 
 	args = parser.parse_args()
 
-	results_path = os.path.join(config.DIR_NAME, "inference_exp_ucb_%s.csv"%(args.model_id))
-	df_result = pd.read_csv(results_path)
-	df_result = df_result.loc[:, ~df_result.columns.str.contains('^Unnamed')]
+	inference_data_path = os.path.join(config.DIR_NAME, "inference_data", args.dataset_name, args.model_name, 
+		"%s_inference_data_%s_%s_branches_id_%s.csv"%(args.calib_type, args.distortion_type, args.n_branches, args.model_id))
+
+	savePath = os.path.join(config.DIR_NAME, "ucb_results", args.dataset_name, args.model_name, 
+		"ucb_results_%s_%s_%s_branches_id_%s.csv"%(args.calib_type, args.model_name, args.n_branches, args.model_id))
+
+	logPath = os.path.join(config.DIR_NAME, "logUCB_id_%s.txt"%(args.model_id))
+
+	df_inf_data = pd.read_csv(inference_data_path)
+	df_inf_data = df_inf_data.loc[:, ~df_inf_data.columns.str.contains('^Unnamed')]
+
 	threshold_list = np.arange(0, 1.1, config.step_arms)
 	overhead_list = np.arange(0, 1.1, config.step_overhead)
 
-	blur_level = [1, 2, 3]
-
-
-	savePath = os.path.join(".", "ucb_bin_delta_conf_result_c_%s_id_%s.csv"%(args.c, args.model_id))
-	logPath = os.path.join(".", "logUCBDeltaConfBin_%s.txt"%(args.model_id))
+	distortion_values = config.distortion_lvl_dict[args.distortion_type]
 
 	logging.basicConfig(level=logging.DEBUG, filename=logPath, filemode="a+", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-	ucb_experiment(df_result, threshold_list, overhead_list, args.n_rounds, args.c, savePath, logPath, verbose)
+	ucb_experiment(df_inf_data, threshold_list, overhead_list, args.n_rounds, args.c, savePath, logPath, verbose)
