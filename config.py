4e6761008ae7ab8a4b077d85af09e60c0eef38d9
Roberto Pacheco <robertopvc@gmail.com>
2022-10-24 07:45:52 -0300

    b_mobilenet with 3 branches

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ pretrained = True
 seed = 42
 batch_size_train = 64
 
-nr_class_dict = {"caltech256": 256}
+nr_class_dict = {"caltech256": 258}
 
 dataset_path_dict = {"caltech256": os.path.join(DIR_NAME, "datasets", "caltech256", "256_ObjectCategories"),
 "cifar10": os.path.join(DIR_NAME,"datasets", "cifar10") }
