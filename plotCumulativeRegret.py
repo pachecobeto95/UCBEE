d6702c4a24c6e6de53e30fbfd94c9ca821ed69f1
Roberto Pacheco <robertopvc@gmail.com>
2022-10-25 11:20:21 -0300

    Update plotCumulativeRegret.py

--- plotCumulativeRegret.py
+++ plotCumulativeRegret.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os, sys, config
 
 saveDir = os.path.join(".", "ucb_results", "caltech256", "mobilenet")
+fontsize = 16
 
 ucb_filename = os.path.join(saveDir, "ucb_results_no_calib_mobilenet_1_branches_id_2.csv")
 fixed_filename = os.path.join(saveDir, "fixed_results_no_calib_mobilenet_1_branches_id_2.csv")
@@ -21,6 +22,7 @@ df_random = df_random.loc[:, ~df_random.columns.str.contains('^Unnamed')]
 overhead_list = np.arange(0, 1.1, config.step_overhead)
 
 for overhead in overhead_list:
+  fig, ax = plt.subplots()
 
   df_ucb_pristine = df_ucb[(df_ucb.distortion_type == "pristine") & (df_ucb.overhead == overhead)]
   df_fixed_pristine = df_fixed[(df_fixed.distortion_type == "pristine") & (df_fixed.overhead == overhead)]
@@ -28,8 +30,8 @@ for overhead in overhead_list:
 
   history = np.arange(1, len(df_ucb_pristine.cumulative_regret.values) + 1)
 
-  plt.plot(history, df_ucb_pristine.cumulative_regret.values)
-  #plt.plot(history, df_fixed_pristine.cumulative_regret.values)
-  plt.plot(history, df_random_pristine.cumulative_regret.values)
-
+  plt.plot(history, df_ucb_pristine.cumulative_regret.values, label="Pristine UCB")
+  #plt.plot(history, df_fixed_pristine.cumulative_regret.values, label="Pristine Fixed")
+  plt.plot(history, df_random_pristine.cumulative_regret.values, label="Pristine Random")
+  plt.legend(frameon=False, fontsize=fontsize)
   plt.savefig("cumulative_results_overhead_%s.pdf"%(overhead) )
