afdb1775cb404e93ca03548a623462a9bfa44073
Roberto Pacheco <robertopvc@gmail.com>
2022-10-25 12:27:56 -0300

    Update plotCumulativeRegret.py

--- plotCumulativeRegret.py
+++ plotCumulativeRegret.py
@@ -20,6 +20,7 @@ df_random = pd.read_csv(random_filename)
 df_random = df_random.loc[:, ~df_random.columns.str.contains('^Unnamed')]
 
 overhead_list = np.arange(0, 1.1, config.step_overhead)
+distortion_list = df_ucb[df_ucb.distortion_type == "gaussian_blur"].distortion_lvl.unique()
 
 for overhead in overhead_list:
   fig, ax = plt.subplots()
@@ -33,6 +34,12 @@ for overhead in overhead_list:
   plt.plot(history, df_ucb_pristine.cumulative_regret.values, label="Pristine UCB")
   #plt.plot(history, df_fixed_pristine.cumulative_regret.values, label="Pristine Fixed")
   plt.plot(history, df_random_pristine.cumulative_regret.values, label="Pristine Random")
+   
+  for distortion_lvl in distortion_list:
+    df_ucb_blur = df_ucb[(df_ucb.distortion_type == "gaussian_blur") & (df_ucb.overhead == overhead) & (df_ucb.distortion_lvl==distortion_lvl)]
+    plt.plot(history, df_ucb_blur.cumulative_regret.values, label=r"UCB Blur $\sigma=%s$"%(distortion_lvl))
+  
+  
   plt.legend(frameon=False, fontsize=fontsize)
   ax.tick_params(axis='both', which='major', labelsize=fontsize)
   plt.ylabel("Cumulative Regret", fontsize=fontsize)
