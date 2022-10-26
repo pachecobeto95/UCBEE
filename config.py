f7cc08b301eddae4b6bb85b23073e7b1e9af0f76
Roberto Pacheco <robertopvc@gmail.com>
2022-10-26 10:53:06 -0300

    plot cumulative regret

--- config.py
+++ config.py
@@ -31,3 +31,5 @@ indices_path_dict = {"caltech256": os.path.join(DIR_NAME, "indices", "caltech256
 "cifar10": os.path.join(DIR_NAME,"indices", "cifar10") }
 
 distortion_lvl_dict = {"pristine": [0], "gaussian_blur": [1, 2, 3, 4, 5], "gaussian_noise": [5, 10, 20, 30, 40]}
+
+fontsize = 16
