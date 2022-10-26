d5b77de3be2df18a6d68df121b2acdd66a6b2091
Roberto Pacheco <robertopvc@gmail.com>
2022-10-26 10:49:57 -0300

    plot cumulative regret

--- plotCumulativeRegret.py
+++ plotCumulativeRegret.py
@@ -3,52 +3,105 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os, sys, config, argparse
 
+#df_random = df_random[(df_random.distortion_type == "pristine") & (df_random.overhead == overhead)]
+
+def extractedData(df):
+
+  df_pristine = df[df.distortion_type == "pristine"] 
+  df_blur = df[df.distortion_type == "gaussian_blur"]
+
+  return df_pristine, df_blur
+
+def cumulativeRegretPlot(df_ucb, df_fixed, df_random, overhead, distortion_list, fontsize, savePath):
 
 
+  df_ucb_pristine, df_ucb_blur = extractedData(df_ucb)
 
-saveDir = os.path.join(".", "ucb_results", "caltech256", "mobilenet")
-fontsize = 16
+  df_fixed_pristine, df_fixed_blur = extractedData(df_fixed)
 
-ucb_filename = os.path.join(saveDir, "ucb_results_no_calib_mobilenet_1_branches_id_2.csv")
-fixed_filename = os.path.join(saveDir, "fixed_results_no_calib_mobilenet_1_branches_id_2.csv")
-random_filename = os.path.join(saveDir, "random_results_no_calib_mobilenet_1_branches_id_2.csv")
+  df_random_pristine, df_random_blur = extractedData(df_random)
 
-df_ucb = pd.read_csv(ucb_filename)
-df_ucb = df_ucb.loc[:, ~df_ucb.columns.str.contains('^Unnamed')] 
+  nr_samples = len(df_ucb_pristine.cumulative_regret.values)
 
-df_fixed = pd.read_csv(fixed_filename)
-df_fixed = df_fixed.loc[:, ~df_fixed.columns.str.contains('^Unnamed')] 
+  history = np.arange(1, nr_samples + 1)
 
-df_random = pd.read_csv(random_filename)
-df_random = df_random.loc[:, ~df_random.columns.str.contains('^Unnamed')]
-
-overhead_list = np.arange(0, 1.1, config.step_overhead)
-distortion_list = df_ucb[df_ucb.distortion_type == "gaussian_blur"].distortion_lvl.unique()
-
-for overhead in overhead_list:
-  fig, ax = plt.subplots()
-
-  df_ucb_pristine = df_ucb[(df_ucb.distortion_type == "pristine") & (df_ucb.overhead == overhead)]
-  df_fixed_pristine = df_fixed[(df_fixed.distortion_type == "pristine") & (df_fixed.overhead == overhead)]
-  df_random_pristine = df_random[(df_random.distortion_type == "pristine") & (df_random.overhead == overhead)]
-
-  history = np.arange(1, len(df_ucb_pristine.cumulative_regret.values) + 1)
-
-  plt.plot(history, df_ucb_pristine.cumulative_regret.values, label="Pristine UCB")
-  #plt.plot(history, df_fixed_pristine.cumulative_regret.values, label="Pristine Fixed")
-  plt.plot(history, df_random_pristine.cumulative_regret.values, label="Pristine Random")
+  plt.plot(history, df_random_pristine.cumulative_regret.values, label="Random Pristine")
+  plt.plot(history, df_ucb_pristine.cumulative_regret.values, label="UCB Pristine")
+  for threshold in threshold_list:
+    df_fixed_pristine_temp = df_fixed_pristine[df_fixed_pristine.threshold==threshold]
+    plt.plot(history, df_fixed_pristine_temp.cumulative_regret.values, label="Pristine Fixed")
    
   for distortion_lvl in distortion_list:
-    df_ucb_blur = df_ucb[(df_ucb.distortion_type == "gaussian_blur") & (df_ucb.overhead == overhead) & (df_ucb.distortion_lvl==distortion_lvl)]
-    plt.plot(history, df_ucb_blur.cumulative_regret.values, label=r"UCB Blur $\sigma=%s$"%(distortion_lvl))
+    df_ucb_blur_temp = df_ucb_blur[df_ucb.distortion_lvl==distortion_lvl]
+    df_random_blur_temp = df_random_blur[df_random_blur.distortion_lvl==distortion_lvl]
+
+    plt.plot(history, df_ucb_blur_temp.cumulative_regret.values, label=r"UCB Blur $\sigma=%s$"%(distortion_lvl))
+    plt.plot(history, df_random_blur_temp.cumulative_regret.values, label=r"Random Blur $\sigma=%s$"%(distortion_lvl))
+
   
-  
+  for threshold in threshold_list:
+    for distortion_lvl in distortion_list:
+      df_fixed_blur_temp = df_fixed_blur[(df_fixed_blur.distortion_lvl==distortion_lvl) & (df_fixed_blur.threshold==threshold)]
+      plt.plot(history, df_fixed_blur_temp.cumulative_regret.values, label=r"Fixed w/$\alpha=%s$ Blur $\sigma=%s$"%(threshold, distortion_lvl))
+
+
   plt.legend(frameon=False, fontsize=fontsize)
   ax.tick_params(axis='both', which='major', labelsize=fontsize)
   plt.ylabel("Cumulative Regret", fontsize=fontsize)
   plt.xlabel("Time Horizon", fontsize=fontsize)
   plt.tight_layout()
-  plt.savefig("cumulative_results_overhead_%s_final.png"%(round(overhead, 2)) )
+  plt.savefig(savePath + ".pdf")
+  #plt.savefig(savePath + ".jpg")
+
+
+def main(args):
+  saveDataDir = os.path.join(config.DIR_NAME, "ucb_results", "caltech256", "mobilenet")
+  savePlotDir = os.path.join(config.DIR_NAME, "plots")
+
+  ucb_filename = os.path.join(saveDataDir, "ucb_results_no_calib_mobilenet_1_branches_id_%s.csv"%(args.model_id))
+  fixed_filename = os.path.join(saveDataDir, "fixed_results_no_calib_mobilenet_1_branches_id_%s.csv"%(args.model_id))
+  random_filename = os.path.join(saveDataDir, "random_results_no_calib_mobilenet_1_branches_id_%s.csv"%(args.model_id) )
+
+  df_ucb = pd.read_csv(ucb_filename)
+  df_ucb = df_ucb.loc[:, ~df_ucb.columns.str.contains('^Unnamed')] 
+
+  df_fixed = pd.read_csv(fixed_filename)
+  df_fixed = df_fixed.loc[:, ~df_fixed.columns.str.contains('^Unnamed')] 
+
+  df_random = pd.read_csv(random_filename)
+  df_random = df_random.loc[:, ~df_random.columns.str.contains('^Unnamed')]
+
+  overhead_list = np.arange(0, 1.1, config.step_overhead)
+  distortion_list = df_ucb[df_ucb.distortion_type == "gaussian_blur"].distortion_lvl.unique()
+
+  for overhead in overhead_list:
+
+    savePath = os.path.join(savePlotDir, "cumulative_results_overhead_%s_test"%(round(overhead, 2)) )
+
+    fig, ax = plt.subplots()
+
+    df_ucb = df_ucb[df_ucb.overhead == overhead]
+    df_fixed = df_fixed[df_fixed.overhead == overhead]
+    df_random = df_random[df_random.overhead == overhead]  
+
+    cumulativeRegretPlot(df_ucb, df_fixed, df_random, overhead, distortion_list, fontsize, savePath)
+
+
+if (__name__ == "__main__"):
+
+  parser = argparse.ArgumentParser(description='Plots the Cumulative Regret Versus Time Horizon for several contexts.')
+  parser.add_argument('--model_id', type=int, default=config.model_id, help='Model Id.')
+  parser.add_argument('--distortion_type', type=str, default=config.distortion_type, help='Distortion Type.')
+  parser.add_argument('--n_branches', type=int, default=config.n_branches, help='Number of exit exits.')
+  parser.add_argument('--dataset_name', type=str, default=config.dataset_name, help='Dataset Name.')
+  parser.add_argument('--model_name', type=str, default=config.model_name, help='Model name.')
+  parser.add_argument('--seed', type=int, default=config.seed, help='Seed.')
+  parser.add_argument('--calib_type', type=str, default="no_calib", help='Calibration type.')
+  parser.add_argument('--fontsize', type=int, default=config.fontsize, help='Font Size.')
+
+  args = parser.parse_args()
+  main(args)
+
 
   
   
