e5f4393252e0b4460ac6f6ead8fa70be10c5c0dc
Roberto Pacheco <robertopvc@gmail.com>
2022-10-26 23:31:13 -0300

    plot cumulative regret

--- run_accuracy_eval_fixed.py
+++ run_accuracy_eval_fixed.py
@@ -118,7 +118,7 @@ if (__name__ == "__main__"):
 	df_inf_data = df_inf_data.loc[:, ~df_inf_data.columns.str.contains('^Unnamed')]
 
 	threshold_list = [0.7, 0.8]
-	distortion_values = [1, 3, 4]
+	distortion_values = [2, 4]
 	overhead_list = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 
