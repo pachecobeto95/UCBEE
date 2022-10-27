fb81c45e2ad475a5c319215b552eca6d24e8e5b8
Roberto Pacheco <robertopvc@gmail.com>
2022-10-27 00:10:21 -0300

    plot cumulative regret

--- run_accuracy_eval_random.py
+++ run_accuracy_eval_random.py
@@ -121,7 +121,7 @@ if (__name__ == "__main__"):
 
 	threshold_list = np.arange(0, 1.1, config.step_arms)
 
-	distortion_values = config.distortion_lvl_dict[args.distortion_type]
+	distortion_values = [1, 2, 3, 4]
 	overhead_list = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 
