5c179954e5bfbb1b56bf05819029f418cdc5f86e
Roberto Pacheco <robertopvc@gmail.com>
2022-10-29 11:35:48 -0300

    fixxing acc issue

--- run_accuracy_eval_ucb.py
+++ run_accuracy_eval_ucb.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import itertools, argparse, os, sys, random, logging, config
 from tqdm import tqdm
-from ucb import ucb, reward_function_1, save_results, save_acc_results
+from ucb import ucb, reward_function_1, reward_function_2, save_results, save_acc_results
 
 
 
@@ -57,11 +57,11 @@ if (__name__ == "__main__"):
 	threshold_list = [0, 0.2, 0.4, 0.6, 0.8, 0.9, 1]
 	#overhead_list = np.arange(0, 1.1, config.step_overhead)
 	#overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15, 0.18, 0.2, 0.23, 0.25, 0.28, 0.3]
-	overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15]
-
+	#overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15]
+	overhead_list = [0]
 	distortion_values_dict = {"pristine": [0], "gaussian_blur": [0.5, 0.8, 1, 2]}
 	distortion_values = distortion_values_dict[args.distortion_type]
 
 
-	run_ucb_inference_eval(args, df_inf_data, reward_function_1, threshold_list, overhead_list, distortion_values, savePath, saveUCBAccPath, 
+	run_ucb_inference_eval(args, df_inf_data, reward_function_2, threshold_list, overhead_list, distortion_values, savePath, saveUCBAccPath, 
 		logPath)
