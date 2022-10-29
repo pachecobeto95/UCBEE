78bfd6417a5279d7f6f225a949132e45bfd18a61
Roberto Pacheco <robertopvc@gmail.com>
2022-10-29 11:45:06 -0300

    fixxing acc issue

--- run_accuracy_eval_random.py
+++ run_accuracy_eval_random.py
@@ -121,7 +121,7 @@ if (__name__ == "__main__"):
 	df_inf_data = df_inf_data.loc[:, ~df_inf_data.columns.str.contains('^Unnamed')]
 
 	threshold_list = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-	overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15]
+	overhead_list = [0]
 
 	distortion_values_dict = {"pristine": [0], "gaussian_blur": [0.5, 0.8, 1, 2]}
 	distortion_values = distortion_values_dict[args.distortion_type]
