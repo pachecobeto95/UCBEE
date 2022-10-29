8e69844354c712509141bf33483b4971b50330f5
Roberto Pacheco <robertopvc@gmail.com>
2022-10-29 20:31:46 -0300

    fixxing acc issue

--- run_accuracy_eval_fixed.py
+++ run_accuracy_eval_fixed.py
@@ -118,11 +118,13 @@ if (__name__ == "__main__"):
 	df_inf_data = df_inf_data.loc[:, ~df_inf_data.columns.str.contains('^Unnamed')]
 
 	threshold_list = [0.7, 0.75, 0.8]
-	#distortion_values = [1, 2, 3, 4]
-	#distortion_values = config.distortion_lvl_dict[args.distortion_type]
-	distortion_values = df_inf_data[df_inf_data.distortion_type == args.distortion_type].distortion_lvl.unique()	
+
+	distortion_values_dict = {"pristine": [0], "gaussian_blur": [0.5, 0.8, 1, 2]}
+	distortion_values = distortion_values_dict[args.distortion_type]
+
 	#overhead_list = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-	overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15, 0.18, 0.2, 0.23, 0.25, 0.28, 0.3]
+	#overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15, 0.18, 0.2, 0.23, 0.25, 0.28, 0.3]
+	overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15]
 
 	ee_inference_fixed_threshold(args, df_inf_data, reward_function_2, threshold_list, overhead_list, distortion_values, savePath, saveUCBAccPath, 
 		logPath)
