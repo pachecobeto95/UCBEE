8a6688bacefd543c85800ad5ec10b938b87184da
Roberto Pacheco <robertopvc@gmail.com>
2022-10-29 09:30:27 -0300

    fixxing acc issue

--- run_accuracy_eval_ucb.py
+++ run_accuracy_eval_ucb.py
@@ -59,7 +59,7 @@ if (__name__ == "__main__"):
 	#overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15, 0.18, 0.2, 0.23, 0.25, 0.28, 0.3]
 	overhead_list = [0, 0.05, 0.08, 0.1, 0.13, 0.15]
 
-	distortion_values = df_inf_data[df_inf_data.distortion_type == args.distortion_type].distortion_lvl.unique()[:-3]
+	distortion_values = df_inf_data[df_inf_data.distortion_type == args.distortion_type].distortion_lvl.unique()
 	print(distortion_values)
 	#run_ucb_inference_eval(args, df_inf_data, reward_function_1, threshold_list, overhead_list, distortion_values, savePath, saveUCBAccPath, 
 	#	logPath)
